4c34f7acac42849c2d122a159761f7decb414609
Радмир Исламов <therad445@outlook.com>
2024-11-15 21:31:12 +0300

    feat: implement LogResult record

---
+++ src/main/java/backend/academy/loganalyzer/template/LogResult.java
@@ -0,0 +1,10 @@
+package backend.academy.loganalyzer.template;
+
+import java.util.Map;
+
+/**
+ * LogResult - класс для работы с данными LogAnalyzer и формирования финального отчета.
+ */
+public record LogResult(long totalRequests, double averageResponseSize, Map<String, Long> resourceCounts,
+                        Map<Integer, Long> statusCodeCounts) {
+}
