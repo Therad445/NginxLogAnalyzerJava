9cc37d96e932b8643573cba3575bb4658fab03dd
Радмир Исламов <therad445@outlook.com>
2024-12-08 12:29:32 +0300

    chore: delete unnecessary comments from Main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -16,28 +16,16 @@ import java.util.stream.Stream;
 import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 
-/**
- * Main.java — запускает приложение,
- * обрабатывает аргументы командной строки и передает их
- * в нужные компоненты.
- */
+
 @Log4j2
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
-
-        // Вввод в JCommander
         Config config = new Config();
         JCommander.newBuilder().addObject(config).build().parse(args);
-
-        //Выбор формата
         LogReportFormat
             formatter = "adoc".equalsIgnoreCase(config.format()) ? new AsciidocFormat() : new MarkdownFormat();
-
-        // Парсинг и анализ логов
         LogResult result = getLogResult(config.path(), config.filterField(), config.filterValue());
-
-        // Формирование и вывод отчета
         log.info(formatter.format(result));
     }
 
@@ -45,24 +33,17 @@ public class Main {
         NginxLogParser parser = new NginxLogParser();
         LogAnalyzer analyzer = new LogAnalyzer();
         try {
-            // Создаем поток
             Stream<String> stringStream = LogPathSelector.typeSelector(path);
-
-            // Обрабатываем поток
             List<LogRecord> logs = parser.parse(stringStream);
-
-            // Проверяем условие
             if (filterField != null && filterValue != null) {
                 logs = analyzer.filterLogs(logs, filterField, filterValue);
             }
 
-            // Содержимое логов
             long totalRequests = analyzer.countTotalRequests(logs);
             double averageSize = analyzer.averageResponseSize(logs);
             Map<String, Long> resourceCounts = analyzer.countResources(logs);
             Map<Integer, Long> statusCodeCounts = analyzer.countStatusCodes(logs);
 
-            // Создание результата анализа
             return new LogResult(totalRequests, averageSize, resourceCounts, statusCodeCounts);
         } catch (Exception e) {
             log.error(e);
