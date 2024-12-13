6707832558086141d27fa5ef335c749d8f1e2996
Радмир Исламов <therad445@outlook.com>
2024-12-13 21:02:16 +0300

    refactor: change LogFilter to OOP implements

---
+++ src/main/java/backend/academy/loganalyzer/analyzer/FieldLogFilter.java
@@ -0,0 +1,28 @@
+package backend.academy.loganalyzer.analyzer;
+
+import backend.academy.loganalyzer.template.LogRecord;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FieldLogFilter implements LogFilter {
+    private final String fieldName;
+    private final String fieldValue;
+
+    public FieldLogFilter(String fieldName, String fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    @Override
+    public List<LogRecord> filter(List<LogRecord> logs) {
+        return logs.stream()
+            .filter(log -> switch (fieldName) {
+                case "method" -> log.method().equalsIgnoreCase(fieldValue);
+                case "agent" -> log.userAgent().startsWith(fieldValue.replace("*", ""));
+                case "status" -> String.valueOf(log.status()).equals(fieldValue);
+                case "request" -> log.request().contains(fieldValue);
+                default -> false;
+            })
+            .collect(Collectors.toList());
+    }
+}
