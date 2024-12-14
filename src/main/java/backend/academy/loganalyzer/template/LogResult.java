54422df278d381ff07d9739ca8ecceb378ae749c
Радмир Исламов <therad445@outlook.com>
2024-12-15 00:24:38 +0300

    feat: implement validations for LogResult values

--- src/main/java/backend/academy/loganalyzer/template/LogResult.java
+++ src/main/java/backend/academy/loganalyzer/template/LogResult.java
@@ -7,4 +7,21 @@ import java.util.Map;
  */
 public record LogResult(long totalRequests, double averageResponseSize, Map<String, Long> resourceCounts,
                         Map<Integer, Long> statusCodeCounts, double percentile) {
+    public LogResult {
+        if (totalRequests < 0) {
+            throw new IllegalArgumentException("totalRequests меньше нуля");
+        }
+        if (averageResponseSize < 0) {
+            throw new IllegalArgumentException("averageResponseSize меньше нуля");
+        }
+        if (resourceCounts == null || resourceCounts.isEmpty()) {
+            throw new NullPointerException("resourceCounts пустой");
+        }
+        if (statusCodeCounts == null || statusCodeCounts.isEmpty()) {
+            throw new NullPointerException("statusCodeCounts пустой");
+        }
+        if (percentile < 0) {
+            throw new IllegalArgumentException("percentile меньше нуля");
+        }
+    }
 }
