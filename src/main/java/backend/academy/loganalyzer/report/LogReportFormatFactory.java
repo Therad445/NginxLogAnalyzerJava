5286fa3c08ef590e4af8c840666967017d355a7c
Радмир Исламов <therad445@outlook.com>
2024-12-14 18:43:42 +0300

    fix: replace "markdows" with "markdown"

--- src/main/java/backend/academy/loganalyzer/report/LogReportFormatFactory.java
+++ src/main/java/backend/academy/loganalyzer/report/LogReportFormatFactory.java
@@ -7,7 +7,7 @@ public class LogReportFormatFactory {
     public static LogReportFormat getLogReportFormat(String format) {
         if ("adoc".equalsIgnoreCase(format)) {
             return new AsciidocFormat();
-        } else if ("markdows".equalsIgnoreCase(format)) {
+        } else if ("markdown".equalsIgnoreCase(format)) {
             return new MarkdownFormat();
         } else {
             throw new IllegalArgumentException("Неизвенстный формат: " + format);
