0b6b4b44e835e4a28639bede21242f73b77c15a1
Радмир Исламов <therad445@outlook.com>
2024-12-14 04:19:48 +0300

    feat: add DateRangeLogFilter

---
+++ src/main/java/backend/academy/loganalyzer/analyzer/DateRangeLogFilter.java
@@ -0,0 +1,23 @@
+package backend.academy.loganalyzer.analyzer;
+
+import backend.academy.loganalyzer.template.LogRecord;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DateRangeLogFilter implements LogFilter {
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    public DateRangeLogFilter(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public List<LogRecord> filter(List<LogRecord> logs) {
+        return logs.stream()
+            .filter(log -> !log.timestamp().isBefore(start) && !log.timestamp().isAfter(end))
+            .collect(Collectors.toList());
+    }
+}
