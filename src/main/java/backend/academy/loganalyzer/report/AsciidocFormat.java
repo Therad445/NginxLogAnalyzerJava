33a55afb1cad81a6e7ab726c09ff6b38dca3a33d
Радмир Исламов <therad445@outlook.com>
2024-12-15 00:25:18 +0300

    feat: implement validations for LogReportFormat value

--- src/main/java/backend/academy/loganalyzer/report/AsciidocFormat.java
+++ src/main/java/backend/academy/loganalyzer/report/AsciidocFormat.java
@@ -5,6 +5,7 @@ import backend.academy.loganalyzer.template.LogResult;
 public class AsciidocFormat implements LogReportFormat {
     @Override
     public String format(LogResult result) {
+        formatNullCheck(result);
         String newLine = "\n";
         String startLine = "== Общая информация\n\n"
             + "|===\n| Метрика | Значение" + newLine;
