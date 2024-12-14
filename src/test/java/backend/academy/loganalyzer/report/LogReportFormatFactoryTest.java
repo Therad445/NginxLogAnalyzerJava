3c0e24c03b239e9738d58f968bd05c59befc6ade
Радмир Исламов <therad445@outlook.com>
2024-12-15 00:28:19 +0300

    test: implement tests for LogReportFormatFactory

---
+++ src/test/java/backend/academy/loganalyzer/report/LogReportFormatFactoryTest.java
@@ -0,0 +1,49 @@
+package backend.academy.loganalyzer.report;
+
+import org.junit.jupiter.api.Test;
+
+import static backend.academy.loganalyzer.report.LogReportFormatFactory.getLogReportFormat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogReportFormatFactoryTest {
+
+    @Test
+    void getLogReportFormatFactory_ShouldReturnMarkdownFormat_WhenFormatIsAdoc() {
+        //Arrange
+        String format = "adoc";
+        //Act
+        LogReportFormat logReportFormat = getLogReportFormat(format);
+        //Assert
+        assertEquals(AsciidocFormat.class, logReportFormat.getClass());
+    }
+
+    @Test
+    void getLogReportFormatFactory_ShouldReturnMarkdownFormat_WhenFormatIsMarkdown() {
+        //Arrange
+        String format = "markdown";
+        //Act
+        LogReportFormat logReportFormat = getLogReportFormat(format);
+        //Assert
+        assertEquals(MarkdownFormat.class, logReportFormat.getClass());
+    }
+
+    @Test
+    void getLogReportFormatFactory_ShouldReturnMarkdownFormat_WhenFormatIsNull() {
+        //Arrange
+        String format = null;
+        //Act
+        LogReportFormat logReportFormat = getLogReportFormat(format);
+        //Assert
+        assertEquals(AsciidocFormat.class, logReportFormat.getClass());
+    }
+
+    @Test
+    void getLogReportFormatFactory_ShouldReturnMarkdownFormat_WhenFormatIsIncorrect() {
+        //Arrange
+        String format = "mistake";
+        //Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> getLogReportFormat(format));
+        //Assert
+        assertEquals("Неизвенстный формат: " + format, exception.getMessage());
+    }
+}
