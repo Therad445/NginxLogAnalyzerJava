77d88e484036f1110c389cc7ac0ffe5db074b269
Радмир Исламов <therad445@outlook.com>
2025-05-10 02:47:21 +0300

    refactor: split main to 3 classes

---
+++ src/main/java/backend/academy/loganalyzer/alert/buildAlertManager.java
@@ -0,0 +1,20 @@
+package backend.academy.loganalyzer.alert;
+
+import java.io.File;
+
+public class buildAlertManager {
+    public static AlertManager buildAlertManager() {
+        String token = System.getenv("TG_TOKEN");
+        String chat = System.getenv("TG_CHAT");
+        if (token != null && chat != null && !token.isBlank() && !chat.isBlank()) {
+            return new TelegramAlertManager(token, chat);
+        }
+        return new AlertManager() {
+            @Override public void send(String text) {
+            }
+
+            @Override public void sendImage(File image, String caption) {
+            }
+        };
+    }
+}
